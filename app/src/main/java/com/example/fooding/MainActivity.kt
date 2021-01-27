8e952177a81b794f7128c859a5686f6d97b5ee54
stopkite <62979643+stopkite@users.noreply.github.com>
2021-01-27 23:42:24 +0900

    테스트 작업

--- app/src/main/java/com/example/fooding/MainActivity.kt
+++ app/src/main/java/com/example/fooding/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //깃 커밋 테스트 중
+        // 세정아 안녕? ㅎㅎㅎ
     }
 }
