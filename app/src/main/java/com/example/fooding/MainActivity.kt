505c76038e958bcaf972b0f85f767e738a016052
stopkite <62979643+stopkite@users.noreply.github.com>
2021-01-29 00:18:37 +0900

    1.activity_main.xml에 Reclcyerview구현 2.list_item.xml 추가 3.java에 ui폴더/adapter 생성함 -> 추후에 파일이 많아졌을 때 혼동을 대비하기 위함

--- app/src/main/java/com/example/fooding/MainActivity.kt
+++ app/src/main/java/com/example/fooding/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //깃 커밋 테스트 중
+        
+        //깃 테스트 중
         // 세정아 안녕? ㅎㅎㅎ
 
         //와 이거 짱신기해ㅐㅇㅁㅇ
         //commit message 으디다 작성하는거신가
     }
+
 }
